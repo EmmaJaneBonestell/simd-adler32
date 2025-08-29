f0bc053da635556014952d244304967ed60734ff
Emma Jane Bonestell <EmmaJaneBonestell@gmail.com>
2025-08-28 23:18:16 -0500

    Fix dotprod cfg_attr.

--- src/imp/neon.rs
+++ src/imp/neon.rs
@@ -1,243 +1,237 @@
 use super::Adler32Imp;
 
-/// Resolves update implementation if CPU supports avx512f and avx512bw instructions.
-pub fn get_imp() -> Option<Adler32Imp> {
-  get_imp_inner()
-}
+/// Resolves update implementation if CPU supports avx512f and avx512bw
+/// instructions.
+pub fn get_imp() -> Option<Adler32Imp> { get_imp_inner() }
 
 #[inline]
 #[cfg(all(
-  feature = "std",
-  feature = "nightly",
-  target_arch = "arm",
-  target_feature = "v7"
-))]
-fn get_imp_inner() -> Option<Adler32Imp> {
-  if std::arch::is_arm_feature_detected!("neon") {
-    Some(imp::update)
-  } else {
-    None
-  }
-}
-
-#[inline]
-#[cfg(all(feature = "std", target_arch = "aarch64"))]
-fn get_imp_inner() -> Option<Adler32Imp> {
-  if std::arch::is_aarch64_feature_detected!("neon") {
-    Some(imp::update)
-  } else {
-    None
-  }
-}
-
-#[inline]
-#[cfg(all(
-  target_feature = "neon",
-  not(all(
     feature = "std",
-    any(
-      all(feature = "nightly", target_arch = "arm", target_feature = "v7"),
-      target_arch = "aarch64"
-    )
-  ))
+    feature = "nightly",
+    target_arch = "arm",
+    target_feature = "v7"
 ))]
 fn get_imp_inner() -> Option<Adler32Imp> {
-  Some(imp::update)
+    if std::arch::is_arm_feature_detected!("neon") {
+        Some(imp::update)
+    } else {
+        None
+    }
+}
+
+#[inline]
+#[cfg(all(feature = "std", feature = "nightly", target_arch = "aarch64"))]
+fn get_imp_inner() -> Option<Adler32Imp> {
+    if std::arch::is_aarch64_feature_detected!("neon")
+        && std::arch::is_aarch64_feature_detected!("dotprod")
+    {
+        Some(imp::update)
+    } else {
+        None
+    }
 }
 
 #[inline]
 #[cfg(all(
-  not(target_feature = "neon"),
-  not(all(
-    feature = "std",
-    any(
-      all(feature = "nightly", target_arch = "arm", target_feature = "v7"),
-      target_arch = "aarch64"
-    )
-  ))
+    feature = "nightly",
+    target_feature = "neon",
+    target_feature = "dotprod",
+    not(all(
+        feature = "std",
+        any(
+            all(target_arch = "arm", target_feature = "v7"),
+            target_arch = "aarch64",
+        )
+    ))
 ))]
-fn get_imp_inner() -> Option<Adler32Imp> {
-  None
-}
+fn get_imp_inner() -> Option<Adler32Imp> { Some(imp::update) }
+
+#[inline]
+#[cfg(all(
+    not(target_feature = "neon"),
+    not(all(
+        feature = "std",
+        feature = "nightly",
+        any(
+            all(target_arch = "arm", target_feature = "v7"),
+            all(target_arch = "aarch64", target_feature = "dotprod"),
+        )
+    ))
+))]
+fn get_imp_inner() -> Option<Adler32Imp> { None }
 
 #[cfg(all(
-  any(
-    all(feature = "nightly", target_arch = "arm", target_feature = "v7"),
-    target_arch = "aarch64"
-  ),
-  any(feature = "std", target_feature = "neon")
+    feature = "nightly",
+    any(
+        all(target_arch = "arm", target_feature = "v7"),
+        target_arch = "aarch64"
+    ),
+    any(feature = "std", target_feature = "neon")
 ))]
 mod imp {
-  const MOD: u32 = 65521;
-  const NMAX: usize = 5552;
-  const BLOCK_SIZE: usize = 32;
-  const CHUNK_SIZE: usize = NMAX / BLOCK_SIZE * BLOCK_SIZE;
+    const MOD: u32 = 65521;
+    const NMAX: usize = 5552;
+    const BLOCK_SIZE: usize = 32;
+    const CHUNK_SIZE: usize = NMAX / BLOCK_SIZE * BLOCK_SIZE;
 
-  #[cfg(target_arch = "aarch64")]
-  use core::arch::aarch64::*;
-  #[cfg(target_arch = "arm")]
-  use core::arch::arm::*;
+    #[cfg(target_arch = "aarch64")]
+    use core::arch::aarch64::*;
+    #[cfg(target_arch = "arm")]
+    use core::arch::arm::*;
 
-  pub fn update(a: u16, b: u16, data: &[u8]) -> (u16, u16) {
-    unsafe { update_imp(a, b, data) }
-  }
+    pub fn update(a: u16, b: u16, data: &[u8]) -> (u16, u16) { unsafe { update_imp(a, b, data) } }
 
-  #[inline]
-  #[cfg_attr(target_arch = "arm", target_feature(enable = "v7"))]
-  #[target_feature(enable = "neon")]
-  unsafe fn update_imp(a: u16, b: u16, data: &[u8]) -> (u16, u16) {
-    let mut a = a as u32;
-    let mut b = b as u32;
+    #[inline]
+    #[cfg_attr(target_arch = "arm", target_feature(enable = "v7"))]
+    #[target_feature(enable = "neon")]
+    unsafe fn update_imp(a: u16, b: u16, data: &[u8]) -> (u16, u16) {
+        let mut a = a as u32;
+        let mut b = b as u32;
 
-    let chunks = data.chunks_exact(CHUNK_SIZE);
-    let remainder = chunks.remainder();
-    for chunk in chunks {
-      update_chunk_block(&mut a, &mut b, chunk);
+        let chunks = data.chunks_exact(CHUNK_SIZE);
+        let remainder = chunks.remainder();
+        for chunk in chunks {
+            update_chunk_block(&mut a, &mut b, chunk);
+        }
+
+        update_block(&mut a, &mut b, remainder);
+
+        (a as u16, b as u16)
     }
 
-    update_block(&mut a, &mut b, remainder);
+    #[inline]
+    unsafe fn update_chunk_block(a: &mut u32, b: &mut u32, chunk: &[u8]) {
+        debug_assert_eq!(
+            chunk.len(),
+            CHUNK_SIZE,
+            "Unexpected chunk size (expected {}, got {})",
+            CHUNK_SIZE,
+            chunk.len()
+        );
 
-    (a as u16, b as u16)
-  }
+        reduce_add_blocks(a, b, chunk);
 
-  #[inline]
-  unsafe fn update_chunk_block(a: &mut u32, b: &mut u32, chunk: &[u8]) {
-    debug_assert_eq!(
-      chunk.len(),
-      CHUNK_SIZE,
-      "Unexpected chunk size (expected {}, got {})",
-      CHUNK_SIZE,
-      chunk.len()
-    );
-
-    reduce_add_blocks(a, b, chunk);
-
-    *a %= MOD;
-    *b %= MOD;
-  }
-
-  #[inline]
-  unsafe fn update_block(a: &mut u32, b: &mut u32, chunk: &[u8]) {
-    debug_assert!(
-      chunk.len() <= CHUNK_SIZE,
-      "Unexpected chunk size (expected <= {}, got {})",
-      CHUNK_SIZE,
-      chunk.len()
-    );
-
-    for byte in reduce_add_blocks(a, b, chunk) {
-      *a += *byte as u32;
-      *b += *a;
+        *a %= MOD;
+        *b %= MOD;
     }
 
-    *a %= MOD;
-    *b %= MOD;
-  }
+    #[inline]
+    unsafe fn update_block(a: &mut u32, b: &mut u32, chunk: &[u8]) {
+        debug_assert!(
+            chunk.len() <= CHUNK_SIZE,
+            "Unexpected chunk size (expected <= {}, got {})",
+            CHUNK_SIZE,
+            chunk.len()
+        );
 
-  #[inline(always)]
-  unsafe fn reduce_add_blocks<'a>(a: &mut u32, b: &mut u32, chunk: &'a [u8]) -> &'a [u8] {
-    if chunk.len() < BLOCK_SIZE {
-      return chunk;
+        for byte in reduce_add_blocks(a, b, chunk) {
+            *a += *byte as u32;
+            *b += *a;
+        }
+
+        *a %= MOD;
+        *b %= MOD;
     }
 
-    let blocks = chunk.chunks_exact(BLOCK_SIZE);
-    let blocks_remainder = blocks.remainder();
+    #[inline(always)]
+    unsafe fn reduce_add_blocks<'a>(a: &mut u32, b: &mut u32, chunk: &'a [u8]) -> &'a [u8] {
+        if chunk.len() < BLOCK_SIZE {
+            return chunk;
+        }
 
-    let weight_hi_v = get_weight_hi();
-    let weight_lo_v = get_weight_lo();
+        let blocks = chunk.chunks_exact(BLOCK_SIZE);
+        let blocks_remainder = blocks.remainder();
 
-    let mut p_v: uint32x4_t = core::mem::transmute([*a * blocks.len() as u32, 0, 0, 0]);
-    let mut a_v: uint32x4_t = core::mem::transmute([0u32, 0, 0, 0]);
-    let mut b_v: uint32x4_t = core::mem::transmute([*b, 0, 0, 0]);
+        let weight_hi_v = get_weight_hi();
+        let weight_lo_v = get_weight_lo();
 
-    for block in blocks {
-      let block_ptr = block.as_ptr() as *const uint8x16_t;
-      let v_lo = core::ptr::read_unaligned(block_ptr);
-      let v_hi = core::ptr::read_unaligned(block_ptr.add(1));
+        let mut p_v: uint32x4_t = core::mem::transmute([*a * blocks.len() as u32, 0, 0, 0]);
+        let mut a_v: uint32x4_t = core::mem::transmute([0u32, 0, 0, 0]);
+        let mut b_v: uint32x4_t = core::mem::transmute([*b, 0, 0, 0]);
 
-      p_v = vaddq_u32(p_v, a_v);
+        for block in blocks {
+            let block_ptr = block.as_ptr() as *const uint8x16_t;
+            let v_lo = core::ptr::read_unaligned(block_ptr);
+            let v_hi = core::ptr::read_unaligned(block_ptr.add(1));
 
-      a_v = vaddq_u32(a_v, vqaddlq_u8(v_lo));
-      b_v = vdotq_u32(b_v, v_lo, weight_lo_v);
+            p_v = vaddq_u32(p_v, a_v);
 
-      a_v = vaddq_u32(a_v, vqaddlq_u8(v_hi));
-      b_v = vdotq_u32(b_v, v_hi, weight_hi_v);
+            a_v = vaddq_u32(a_v, vqaddlq_u8(v_lo));
+
+            a_v = vaddq_u32(a_v, vqaddlq_u8(v_hi));
+            b_v = vdotq_u32(b_v, v_hi, weight_hi_v);
+        }
+
+        b_v = vaddq_u32(b_v, vshlq_n_u32(p_v, 5));
+
+        *a += vaddvq_u32(a_v);
+        *b = vaddvq_u32(b_v);
+
+        blocks_remainder
     }
 
-    b_v = vaddq_u32(b_v, vshlq_n_u32(p_v, 5));
+    #[inline(always)]
+    unsafe fn vqaddlq_u8(a: uint8x16_t) -> uint32x4_t { vpaddlq_u16(vpaddlq_u8(a)) }
 
-    *a += vaddvq_u32(a_v);
-    *b = vaddvq_u32(b_v);
+    #[inline(always)]
+    unsafe fn get_weight_lo() -> uint8x16_t {
+        core::mem::transmute([
+            32u8, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17,
+        ])
+    }
 
-    blocks_remainder
-  }
-
-  #[inline(always)]
-  unsafe fn vqaddlq_u8(a: uint8x16_t) -> uint32x4_t {
-    vpaddlq_u16(vpaddlq_u8(a))
-  }
-
-  #[inline(always)]
-  unsafe fn get_weight_lo() -> uint8x16_t {
-    core::mem::transmute([
-      32u8, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17,
-    ])
-  }
-
-  #[inline(always)]
-  unsafe fn get_weight_hi() -> uint8x16_t {
-    core::mem::transmute([16u8, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
-  }
+    #[inline(always)]
+    unsafe fn get_weight_hi() -> uint8x16_t {
+        core::mem::transmute([16u8, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
+    }
 }
 
 #[cfg(test)]
 mod tests {
-  use rand::Rng;
+    use rand::Rng;
 
-  #[test]
-  fn zeroes() {
-    assert_sum_eq(&[]);
-    assert_sum_eq(&[0]);
-    assert_sum_eq(&[0, 0]);
-    assert_sum_eq(&[0; 100]);
-    assert_sum_eq(&[0; 1024]);
-    assert_sum_eq(&[0; 1024 * 1024]);
-  }
-
-  #[test]
-  fn ones() {
-    assert_sum_eq(&[]);
-    assert_sum_eq(&[1]);
-    assert_sum_eq(&[1, 1]);
-    assert_sum_eq(&[1; 100]);
-    assert_sum_eq(&[1; 1024]);
-    assert_sum_eq(&[1; 1024 * 1024]);
-  }
-
-  #[test]
-  fn random() {
-    let mut random = [0; 1024 * 1024];
-    rand::thread_rng().fill(&mut random[..]);
-
-    assert_sum_eq(&random[..1]);
-    assert_sum_eq(&random[..100]);
-    assert_sum_eq(&random[..1024]);
-    assert_sum_eq(&random[..1024 * 1024]);
-  }
-
-  /// Example calculation from https://en.wikipedia.org/wiki/Adler-32.
-  #[test]
-  fn wiki() {
-    assert_sum_eq(b"Wikipedia");
-  }
-
-  fn assert_sum_eq(data: &[u8]) {
-    if let Some(update) = super::get_imp() {
-      let (a, b) = update(1, 0, data);
-      let left = u32::from(b) << 16 | u32::from(a);
-      let right = adler::adler32_slice(data);
-
-      assert_eq!(left, right, "len({})", data.len());
+    #[test]
+    fn zeroes() {
+        assert_sum_eq(&[]);
+        assert_sum_eq(&[0]);
+        assert_sum_eq(&[0, 0]);
+        assert_sum_eq(&[0; 100]);
+        assert_sum_eq(&[0; 1024]);
+        assert_sum_eq(&[0; 1024 * 1024]);
     }
-  }
+
+    #[test]
+    fn ones() {
+        assert_sum_eq(&[]);
+        assert_sum_eq(&[1]);
+        assert_sum_eq(&[1, 1]);
+        assert_sum_eq(&[1; 100]);
+        assert_sum_eq(&[1; 1024]);
+        assert_sum_eq(&[1; 1024 * 1024]);
+    }
+
+    #[test]
+    fn random() {
+        let mut random = [0; 1024 * 1024];
+        rand::thread_rng().fill(&mut random[..]);
+
+        assert_sum_eq(&random[..1]);
+        assert_sum_eq(&random[..100]);
+        assert_sum_eq(&random[..1024]);
+        assert_sum_eq(&random[..1024 * 1024]);
+    }
+
+    /// Example calculation from https://en.wikipedia.org/wiki/Adler-32.
+    #[test]
+    fn wiki() { assert_sum_eq(b"Wikipedia"); }
+
+    fn assert_sum_eq(data: &[u8]) {
+        if let Some(update) = super::get_imp() {
+            let (a, b) = update(1, 0, data);
+            let left = u32::from(b) << 16 | u32::from(a);
+            let right = adler::adler32_slice(data);
+
+            assert_eq!(left, right, "len({})", data.len());
+        }
+    }
 }
--- src/imp/wasm.rs
+++ src/imp/wasm.rs
@@ -1,226 +1,218 @@
 use super::Adler32Imp;
 
 /// Resolves update implementation if CPU supports simd128 instructions.
-pub fn get_imp() -> Option<Adler32Imp> {
-  get_imp_inner()
-}
+pub fn get_imp() -> Option<Adler32Imp> { get_imp_inner() }
 
 #[inline]
 #[cfg(target_feature = "simd128")]
-fn get_imp_inner() -> Option<Adler32Imp> {
-  Some(imp::update)
-}
+fn get_imp_inner() -> Option<Adler32Imp> { Some(imp::update) }
 
 #[inline]
 #[cfg(not(target_feature = "simd128"))]
-fn get_imp_inner() -> Option<Adler32Imp> {
-  None
-}
+fn get_imp_inner() -> Option<Adler32Imp> { None }
 
 #[cfg(target_feature = "simd128")]
 mod imp {
-  const MOD: u32 = 65521;
-  const NMAX: usize = 5552;
-  const BLOCK_SIZE: usize = 32;
-  const CHUNK_SIZE: usize = NMAX / BLOCK_SIZE * BLOCK_SIZE;
+    const MOD: u32 = 65521;
+    const NMAX: usize = 5552;
+    const BLOCK_SIZE: usize = 32;
+    const CHUNK_SIZE: usize = NMAX / BLOCK_SIZE * BLOCK_SIZE;
 
-  #[cfg(target_arch = "wasm32")]
-  use core::arch::wasm32::*;
-  #[cfg(target_arch = "wasm64")]
-  use core::arch::wasm64::*;
+    #[cfg(target_arch = "wasm32")]
+    use core::arch::wasm32::*;
+    #[cfg(target_arch = "wasm64")]
+    use core::arch::wasm64::*;
 
-  pub fn update(a: u16, b: u16, data: &[u8]) -> (u16, u16) {
-    update_imp(a, b, data)
-  }
+    pub fn update(a: u16, b: u16, data: &[u8]) -> (u16, u16) { update_imp(a, b, data) }
 
-  #[inline]
-  #[target_feature(enable = "simd128")]
-  fn update_imp(a: u16, b: u16, data: &[u8]) -> (u16, u16) {
-    let mut a = a as u32;
-    let mut b = b as u32;
+    #[inline]
+    #[target_feature(enable = "simd128")]
+    fn update_imp(a: u16, b: u16, data: &[u8]) -> (u16, u16) {
+        let mut a = a as u32;
+        let mut b = b as u32;
 
-    let chunks = data.chunks_exact(CHUNK_SIZE);
-    let remainder = chunks.remainder();
-    for chunk in chunks {
-      update_chunk_block(&mut a, &mut b, chunk);
+        let chunks = data.chunks_exact(CHUNK_SIZE);
+        let remainder = chunks.remainder();
+        for chunk in chunks {
+            update_chunk_block(&mut a, &mut b, chunk);
+        }
+
+        update_block(&mut a, &mut b, remainder);
+
+        (a as u16, b as u16)
     }
 
-    update_block(&mut a, &mut b, remainder);
+    fn update_chunk_block(a: &mut u32, b: &mut u32, chunk: &[u8]) {
+        debug_assert_eq!(
+            chunk.len(),
+            CHUNK_SIZE,
+            "Unexpected chunk size (expected {}, got {})",
+            CHUNK_SIZE,
+            chunk.len()
+        );
 
-    (a as u16, b as u16)
-  }
+        reduce_add_blocks(a, b, chunk);
 
-  fn update_chunk_block(a: &mut u32, b: &mut u32, chunk: &[u8]) {
-    debug_assert_eq!(
-      chunk.len(),
-      CHUNK_SIZE,
-      "Unexpected chunk size (expected {}, got {})",
-      CHUNK_SIZE,
-      chunk.len()
-    );
-
-    reduce_add_blocks(a, b, chunk);
-
-    *a %= MOD;
-    *b %= MOD;
-  }
-
-  fn update_block(a: &mut u32, b: &mut u32, chunk: &[u8]) {
-    debug_assert!(
-      chunk.len() <= CHUNK_SIZE,
-      "Unexpected chunk size (expected <= {}, got {})",
-      CHUNK_SIZE,
-      chunk.len()
-    );
-
-    for byte in reduce_add_blocks(a, b, chunk) {
-      *a += *byte as u32;
-      *b += *a;
+        *a %= MOD;
+        *b %= MOD;
     }
 
-    *a %= MOD;
-    *b %= MOD;
-  }
+    fn update_block(a: &mut u32, b: &mut u32, chunk: &[u8]) {
+        debug_assert!(
+            chunk.len() <= CHUNK_SIZE,
+            "Unexpected chunk size (expected <= {}, got {})",
+            CHUNK_SIZE,
+            chunk.len()
+        );
 
-  #[inline(always)]
-  fn reduce_add_blocks<'a>(a: &mut u32, b: &mut u32, chunk: &'a [u8]) -> &'a [u8] {
-    if chunk.len() < BLOCK_SIZE {
-      return chunk;
+        for byte in reduce_add_blocks(a, b, chunk) {
+            *a += *byte as u32;
+            *b += *a;
+        }
+
+        *a %= MOD;
+        *b %= MOD;
     }
 
-    let blocks = chunk.chunks_exact(BLOCK_SIZE);
-    let blocks_remainder = blocks.remainder();
+    #[inline(always)]
+    fn reduce_add_blocks<'a>(a: &mut u32, b: &mut u32, chunk: &'a [u8]) -> &'a [u8] {
+        if chunk.len() < BLOCK_SIZE {
+            return chunk;
+        }
 
-    let weight_hi_v = get_weight_hi();
-    let weight_lo_v = get_weight_lo();
+        let blocks = chunk.chunks_exact(BLOCK_SIZE);
+        let blocks_remainder = blocks.remainder();
 
-    let mut p_v = u32x4(*a * blocks.len() as u32, 0, 0, 0);
-    let mut a_v = u32x4(0, 0, 0, 0);
-    let mut b_v = u32x4(*b, 0, 0, 0);
+        let weight_hi_v = get_weight_hi();
+        let weight_lo_v = get_weight_lo();
 
-    for block in blocks {
-      let block_ptr = block.as_ptr() as *const v128;
-      let v_lo = unsafe { block_ptr.read_unaligned() };
-      let v_hi = unsafe { block_ptr.add(1).read_unaligned() };
+        let mut p_v = u32x4(*a * blocks.len() as u32, 0, 0, 0);
+        let mut a_v = u32x4(0, 0, 0, 0);
+        let mut b_v = u32x4(*b, 0, 0, 0);
 
-      p_v = u32x4_add(p_v, a_v);
+        for block in blocks {
+            let block_ptr = block.as_ptr() as *const v128;
+            let v_lo = unsafe { block_ptr.read_unaligned() };
+            let v_hi = unsafe { block_ptr.add(1).read_unaligned() };
 
-      a_v = u32x4_add(a_v, u32x4_extadd_quarters_u8x16(v_lo));
-      let mad = u32x4_dot_u8x16(v_lo, weight_lo_v);
-      b_v = u32x4_add(b_v, mad);
+            p_v = u32x4_add(p_v, a_v);
 
-      a_v = u32x4_add(a_v, u32x4_extadd_quarters_u8x16(v_hi));
-      let mad = u32x4_dot_u8x16(v_hi, weight_hi_v);
-      b_v = u32x4_add(b_v, mad);
+            a_v = u32x4_add(a_v, u32x4_extadd_quarters_u8x16(v_lo));
+            let mad = u32x4_dot_u8x16(v_lo, weight_lo_v);
+            b_v = u32x4_add(b_v, mad);
+
+            a_v = u32x4_add(a_v, u32x4_extadd_quarters_u8x16(v_hi));
+            let mad = u32x4_dot_u8x16(v_hi, weight_hi_v);
+            b_v = u32x4_add(b_v, mad);
+        }
+
+        b_v = u32x4_add(b_v, u32x4_shl(p_v, 5));
+
+        *a += u32x4_horizontal_add(a_v);
+        *b = u32x4_horizontal_add(b_v);
+
+        blocks_remainder
     }
 
-    b_v = u32x4_add(b_v, u32x4_shl(p_v, 5));
+    #[inline(always)]
+    fn u32x4_dot_u8x16(a: v128, b: v128) -> v128 {
+        let a_lo = u16x8_extend_low_u8x16(a);
+        let a_hi = u16x8_extend_high_u8x16(a);
 
-    *a += u32x4_horizontal_add(a_v);
-    *b = u32x4_horizontal_add(b_v);
+        let b_lo = u16x8_extend_low_u8x16(b);
+        let b_hi = u16x8_extend_high_u8x16(b);
 
-    blocks_remainder
-  }
+        // There is no unsigned version for dot. However since we just zero
+        // extended, we know that the upper bytes are 0 and thus the sign extension
+        // involved here before multiplying isn't going to mess with the result.
+        let lo = i32x4_dot_i16x8(a_lo, b_lo);
+        let hi = i32x4_dot_i16x8(a_hi, b_hi);
 
-  #[inline(always)]
-  fn u32x4_dot_u8x16(a: v128, b: v128) -> v128 {
-    let a_lo = u16x8_extend_low_u8x16(a);
-    let a_hi = u16x8_extend_high_u8x16(a);
-
-    let b_lo = u16x8_extend_low_u8x16(b);
-    let b_hi = u16x8_extend_high_u8x16(b);
-
-    // There is no unsigned version for dot. However since we just zero
-    // extended, we know that the upper bytes are 0 and thus the sign extension
-    // involved here before multiplying isn't going to mess with the result.
-    let lo = i32x4_dot_i16x8(a_lo, b_lo);
-    let hi = i32x4_dot_i16x8(a_hi, b_hi);
-
-    u32x4_add(lo, hi)
-  }
-
-  #[inline(always)]
-  fn u32x4_extadd_quarters_u8x16(a: v128) -> v128 {
-    // Technically we want this to be unsigned, but u32x4_extadd_pairwise_u16x8
-    // lowers to pxor, pmaddwd, paddd on x86 whereas i32x4_extadd_pairwise_i16x8
-    // can just be lowered to pmaddwd. However since u16x8_extadd_pairwise_u8x16
-    // already ensures the top byte is mostly zero except for carry, we can just
-    // use the sign extending version instead of the zero extending version
-    // after to ensure better codegen on x86.
-    i32x4_extadd_pairwise_i16x8(u16x8_extadd_pairwise_u8x16(a))
-  }
-
-  #[inline(always)]
-  fn u32x4_horizontal_add(v: v128) -> u32 {
-    let arr: [u32; 4] = unsafe { core::mem::transmute(v) };
-    let mut sum = 0u32;
-    for val in arr {
-      sum = sum.wrapping_add(val);
+        u32x4_add(lo, hi)
     }
-    sum
-  }
 
-  #[inline(always)]
-  fn get_weight_lo() -> v128 {
-    u8x16(
-      32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17,
-    )
-  }
+    #[inline(always)]
+    fn u32x4_extadd_quarters_u8x16(a: v128) -> v128 {
+        // Technically we want this to be unsigned, but u32x4_extadd_pairwise_u16x8
+        // lowers to pxor, pmaddwd, paddd on x86 whereas i32x4_extadd_pairwise_i16x8
+        // can just be lowered to pmaddwd. However since u16x8_extadd_pairwise_u8x16
+        // already ensures the top byte is mostly zero except for carry, we can just
+        // use the sign extending version instead of the zero extending version
+        // after to ensure better codegen on x86.
+        i32x4_extadd_pairwise_i16x8(u16x8_extadd_pairwise_u8x16(a))
+    }
 
-  #[inline(always)]
-  fn get_weight_hi() -> v128 {
-    u8x16(16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
-  }
+    #[inline(always)]
+    fn u32x4_horizontal_add(v: v128) -> u32 {
+        let arr: [u32; 4] = unsafe { core::mem::transmute(v) };
+        let mut sum = 0u32;
+        for val in arr {
+            sum = sum.wrapping_add(val);
+        }
+        sum
+    }
+
+    #[inline(always)]
+    fn get_weight_lo() -> v128 {
+        u8x16(
+            32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17,
+        )
+    }
+
+    #[inline(always)]
+    fn get_weight_hi() -> v128 {
+        u8x16(
+            16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1,
+        )
+    }
 }
 
 #[cfg(test)]
 mod tests {
-  use rand::Rng;
+    use rand::Rng;
 
-  #[test]
-  fn zeroes() {
-    assert_sum_eq(&[]);
-    assert_sum_eq(&[0]);
-    assert_sum_eq(&[0, 0]);
-    assert_sum_eq(&[0; 100]);
-    assert_sum_eq(&[0; 1024]);
-    assert_sum_eq(&[0; 512 * 1024]);
-  }
-
-  #[test]
-  fn ones() {
-    assert_sum_eq(&[]);
-    assert_sum_eq(&[1]);
-    assert_sum_eq(&[1, 1]);
-    assert_sum_eq(&[1; 100]);
-    assert_sum_eq(&[1; 1024]);
-    assert_sum_eq(&[1; 512 * 1024]);
-  }
-
-  #[test]
-  fn random() {
-    let mut random = [0; 512 * 1024];
-    rand::thread_rng().fill(&mut random[..]);
-
-    assert_sum_eq(&random[..1]);
-    assert_sum_eq(&random[..100]);
-    assert_sum_eq(&random[..1024]);
-    assert_sum_eq(&random[..512 * 1024]);
-  }
-
-  /// Example calculation from https://en.wikipedia.org/wiki/Adler-32.
-  #[test]
-  fn wiki() {
-    assert_sum_eq(b"Wikipedia");
-  }
-
-  fn assert_sum_eq(data: &[u8]) {
-    if let Some(update) = super::get_imp() {
-      let (a, b) = update(1, 0, data);
-      let left = u32::from(b) << 16 | u32::from(a);
-      let right = adler::adler32_slice(data);
-
-      assert_eq!(left, right, "len({})", data.len());
+    #[test]
+    fn zeroes() {
+        assert_sum_eq(&[]);
+        assert_sum_eq(&[0]);
+        assert_sum_eq(&[0, 0]);
+        assert_sum_eq(&[0; 100]);
+        assert_sum_eq(&[0; 1024]);
+        assert_sum_eq(&[0; 512 * 1024]);
     }
-  }
+
+    #[test]
+    fn ones() {
+        assert_sum_eq(&[]);
+        assert_sum_eq(&[1]);
+        assert_sum_eq(&[1, 1]);
+        assert_sum_eq(&[1; 100]);
+        assert_sum_eq(&[1; 1024]);
+        assert_sum_eq(&[1; 512 * 1024]);
+    }
+
+    #[test]
+    fn random() {
+        let mut random = [0; 512 * 1024];
+        rand::thread_rng().fill(&mut random[..]);
+
+        assert_sum_eq(&random[..1]);
+        assert_sum_eq(&random[..100]);
+        assert_sum_eq(&random[..1024]);
+        assert_sum_eq(&random[..512 * 1024]);
+    }
+
+    /// Example calculation from https://en.wikipedia.org/wiki/Adler-32.
+    #[test]
+    fn wiki() { assert_sum_eq(b"Wikipedia"); }
+
+    fn assert_sum_eq(data: &[u8]) {
+        if let Some(update) = super::get_imp() {
+            let (a, b) = update(1, 0, data);
+            let left = u32::from(b) << 16 | u32::from(a);
+            let right = adler::adler32_slice(data);
+
+            assert_eq!(left, right, "len({})", data.len());
+        }
+    }
 }
